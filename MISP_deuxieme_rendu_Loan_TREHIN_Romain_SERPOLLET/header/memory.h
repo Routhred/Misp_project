f85ef463f34d823671ebf8ead85871382e3ac4c8
Routhred <romain.serpollet@gmail.com>
2022-01-13 19:30:45 +0100

    deuxieme rendu

---
+++ MISP_deuxieme_rendu_Loan_TREHIN_Romain_SERPOLLET/header/memory.h
@@ -0,0 +1,23 @@
+#ifndef __MEMORY_H__
+#define __MEMORY_H__
+
+#include "struct.h"
+#include "utile.h"
+//fonction pour ecrire dans la memoire, si la valeur fait plus de 8 bits on écrit sur plusieurs adresses
+void ecrireMemoire(char value[],int adresse);
+//fonction pour lire dans la mémoire, on vient lire 32bits sur 4 adresses
+void lireMemoire(char value[], int adresse);
+//fonction qui affiche toute la mémoire utilisée
+void afficherMemoire();
+
+
+
+
+
+
+
+
+
+
+
+#endif
---
+++ MISP_deuxieme_rendu_Loan_TREHIN_Romain_SERPOLLET/header/registre.h
@@ -0,0 +1,25 @@
+#ifndef __REGISTRE_H__
+#define __REGISTRE_H__
+#include "struct.h"
+#include "utile.h"
+#define MAX 200
+//initalise les registres
+void init_registre();
+//ecrire une valeur sur 32 bits dans un registre
+void ecrireRegistre(int registre, char value[]);
+//1 si le registre n'est pas protégé 0 sinon
+int verifRegistre(int registre);
+//affiche toute la memoire registre
+void afficherRegistres();
+//incremente un registre passé en parametre
+void inc(int registre);
+//lit ce qu'il y a dans un registre
+void lireRegistre(int registre, char value[]);
+//stocke les registres dans le fichier de destination dans le dossier "registre/"
+void stockerRegistres(char dest[]);
+
+
+
+
+
+#endif
